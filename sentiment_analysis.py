f415f4ce60ebbb97743fb8acbea0ec00ed0ff829
Lauren Ferrara <lferrara@nd.edu>
2017-03-22 10:17:07 -0400

    made changes to sentiment_analysis.py for presentation

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -16,16 +16,37 @@ BURBERRY_TWEETS_FILE = 'burberry_tweets.json'
 
 # Functions
 # ============================================================================*
+def get_analysis(analyzer, filename):
+        with open(filename) as file:    
+                tweets = json.load(file)
 
+        tot_pos = 0
+        tot_neg = 0
+        tot_comp = 0
+        tot_neutral = 0
+        num_tweets = 0
+
+        for ID in tweets:
+                #if tweets['date']
+                vs = analyzer.polarity_scores(tweets[ID])
+                tot_pos += vs['pos']
+                tot_neg += vs['neg']
+                tot_comp += vs['compound']
+                tot_neutral += vs['neu']
+                num_tweets += 1
+
+        print(filename,':')
+        print('-------------------------------')
+        print('Number of tweets: ', num_tweets)
+        print('Average compound: ', tot_comp/num_tweets)
+        print('Average positive: ', tot_pos/num_tweets)
+        print('Average negative: ', tot_neg/num_tweets)
+        print('Average neutral: ', tot_neutral/num_tweets)
 
 # Main
 # ============================================================================*
 if __name__ == '__main__':
-	analyzer = SentimentIntensityAnalyzer()
-
-	with open(LULULEMON_TWEETS_FILE) as file:    
-		lulu_tweets = json.load(file)
-
-	for ID in lulu_tweets:
-		vs = analyzer.polarity_scores(lulu_tweets[ID])
-		print("{:-<65} {}".format(lulu_tweets[ID], str(vs)))
+        analyzer = SentimentIntensityAnalyzer()
+        get_analysis(analyzer, LULULEMON_TWEETS_FILE)
+        get_analysis(analyzer, URBAN_OUTFITTERS_TWEETS_FILE)
+        get_analysis(analyzer, BURBERRY_TWEETS_FILE)
