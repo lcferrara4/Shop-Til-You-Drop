b8e686e0872debe784b5fe285f19a29acef3f5b1
Lauren Ferrara <lferrara@nd.edu>
2017-04-10 21:09:40 -0400

    updated sentiment_analysis to work with dates

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,6 +6,7 @@
 # ============================================================================*
 import time
 import json
+import os
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # Files
@@ -17,36 +18,44 @@ BURBERRY_TWEETS_FILE = 'burberry_tweets.json'
 # Functions
 # ============================================================================*
 def get_analysis(analyzer, filename):
-        with open(filename) as file:    
-                tweets = json.load(file)
 
         tot_pos = 0
         tot_neg = 0
         tot_comp = 0
         tot_neutral = 0
         num_tweets = 0
-
-        for ID in tweets:
+ 
+        with open(filename) as file:    
+            tweets = json.load(file)
+            for tweet in tweets:
                 #if tweets['date']
-                vs = analyzer.polarity_scores(tweets[ID])
+                vs = analyzer.polarity_scores(tweet['text'])
                 tot_pos += vs['pos']
                 tot_neg += vs['neg']
                 tot_comp += vs['compound']
                 tot_neutral += vs['neu']
                 num_tweets += 1
 
-        print(filename,':')
-        print('-------------------------------')
-        print('Number of tweets: ', num_tweets)
-        print('Average compound: ', tot_comp/num_tweets)
-        print('Average positive: ', tot_pos/num_tweets)
-        print('Average negative: ', tot_neg/num_tweets)
-        print('Average neutral: ', tot_neutral/num_tweets)
+            print(num_tweets)
+            company = filename.split('/')[2].split("_")[0]
+            data_file = "./sentiment_data/" + company + ".csv"
+            f = open(data_file, "a+")
+            date = filename.split('/')[3].split("_")[1]
+            if num_tweets != 0:
+                f.write(date + ',' + str(num_tweets) + ',' + str(tot_comp/num_tweets) + ',' + str(tot_pos/num_tweets) + ',' + str(tot_neg/num_tweets) + ',' + str(tot_neutral/num_tweets) + '\n')
+            else:
+                f.write(date + ',' + str(num_tweets) + ',0,0,0,0\n')
+            f.close()
 
 # Main
 # ============================================================================*
 if __name__ == '__main__':
         analyzer = SentimentIntensityAnalyzer()
-        get_analysis(analyzer, LULULEMON_TWEETS_FILE)
-        get_analysis(analyzer, URBAN_OUTFITTERS_TWEETS_FILE)
-        get_analysis(analyzer, BURBERRY_TWEETS_FILE)
+        data_dir = "./sentiment_data/"
+        for subdir, dirs, files in os.walk("./twitter_data"):
+            #for s in dirs:
+            #    open(data_dir + s, "w+")
+            for f in files:
+                get_analysis(analyzer, os.path.join(subdir, f))
+                #get_analysis(analyzer, URBAN_OUTFITTERS_TWEETS_FILE)
+                #get_analysis(analyzer, BURBERRY_TWEETS_FILE)
