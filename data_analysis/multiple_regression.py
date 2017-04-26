b7b058b4b0b3e963ebef92300d05b1fdca26eeed
Lauren Ferrara <lferrara@nd.edu>
2017-04-26 00:56:42 -0400

    multiple regression with univariate feature selection

--- data_analysis/multiple_regression.py
+++ data_analysis/multiple_regression.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+import sklearn as sklearn
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import SelectKBest, f_regression, chi2
 
 def buildLaggedFeatures(s,lag=2,dropna=True):
     '''
@@ -26,10 +30,11 @@ def buildLaggedFeatures(s,lag=2,dropna=True):
         return res
 
 # create a vector
-brby_data = np.genfromtxt('../sentiment_data/main/burberry_sentiment.csv', delimiter=',')
-lulu_data = np.genfromtxt('../sentiment_data/main/lulu_sentiment.csv', delimiter=',')
-urbn_data = np.genfromtxt('../sentiment_data/main/urban_sentiment.csv', delimiter=',')
-
+#brby_data = np.genfromtxt('../sentiment_data/main/burberry_sentiment.csv', delimiter=',')
+#lulu_data = np.genfromtxt('../sentiment_data/main/lulu_sentiment.csv', delimiter=',')
+#urbn_data = np.genfromtxt('../sentiment_data/main/urban_sentiment.csv', delimiter=',')
+df_urbn = pd.read_csv('urbn2.csv')
+'''
 
 # Burberry
 N = len(brby_data[:,6])
@@ -51,12 +56,38 @@ print("LuLu:")
 with pd.option_context('display.max_rows', 30, 'display.max_columns', 999):
     print(df.corr())
 
+'''
 # Urbn
-N = len(urbn_data[:,6])
-s=pd.DataFrame(urbn_data[:,6], index = list(range(0,N))).astype(float)
-res=buildLaggedFeatures(s,lag=15,dropna=False).astype(float)
-df = pd.DataFrame(urbn_data, columns=list('ABCDEFGHIJK'), index=list(range(0,N))).astype(float)
-df = df.join(res)
-print("Urban:")
+N = len(df_urbn.index)
+#s=pd.DataFrame(df_urbn["Stocks"], index = list(range(0,N))).astype(float)
+s = df_urbn["Stock"].copy()
+res=buildLaggedFeatures(s,lag=15,dropna=False)
+#df = pd.DataFrame(df_urbn, columns=list('ABCDEFGHIJK'), index=list(range(0,N))).astype(float)
+df_urbn = df_urbn.join(res)
+#print("Urban:")
 with pd.option_context('display.max_rows', 30, 'display.max_columns', 999):
-    print(df.corr())
+    print(df_urbn.corr())
+
+df_urbn = df_urbn[13:21]
+X = df_urbn[['TB', 'VaderNeg']]
+y = df_urbn['lag_13']
+
+# fit OLS model
+X = sm.add_constant(X)
+est = sm.OLS(y,X,missing = 'drop').fit()
+print(est.summary())
+
+predictors = ['NumTweets', 'VaderComp', 'VaderPos', 'VaderNeg', 'VaderNeu', 'TB','ImpactVader', 'FollowVader', 'TW', 'TWPub']
+X = df_urbn[predictors]
+result = sklearn.feature_selection.f_regression(X, y, center=True)
+
+scores = result[0]
+pvalues = result[1]
+print(predictors)
+print(scores)
+print(pvalues)
+# Plot the scores.  See how "Pclass", "Sex", "Title", and "Fare" are the best?
+plt.bar(range(len(predictors)), scores)
+plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+plt.show()
+#sklearn.feature_selection.SelectKBest(sklearn.feature_selection.f_regression, k = 3))
